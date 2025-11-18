347ff5c852b10e9400b279c00b65156a398699b5
Muhammadsoban7723 <muhammadsoban8090@gmail.com>
2025-11-18 13:24:12 +0500

    deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
@@ -31,6 +32,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-4PSKX5EG6M"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4PSKX5EG6M');
+          `}
+        </Script>
         <Header />
         <main className="min-h-screen">
           {children}
