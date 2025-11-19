5d1ec25d665fe1925919901470225cad2255baf6
Muhammadsoban7723 <muhammadsoban8090@gmail.com>
2025-11-19 11:53:34 +0500

    add ahref script

--- app/head.tsx
+++ app/head.tsx
@@ -1,11 +1,10 @@
 export default function Head() {
   return (
     <>
-      <script
-        src="https://analytics.ahrefs.com/analytics.js"
-        data-key="NKxXf5xGU+N48wLRNt5C+g"
-        async
-      ></script>
+      <meta
+        name="ahrefs-site-verification"
+        content="d602a92b35f7c67aaa9b9a14c00e5ecae5129902b86b0a75f768076fe4772bed"
+      />
     </>
   )
 }
