0be876d09a881791961a09ca445205f22a8df4dd
Muhammadsoban7723 <muhammadsoban8090@gmail.com>
2025-11-19 11:48:52 +0500

    add ahref code

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <script
+        src="https://analytics.ahrefs.com/analytics.js"
+        data-key="NKxXf5xGU+N48wLRNt5C+g"
+        async
+      ></script>
+    </>
+  )
+}
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -35,14 +35,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          src="https://analytics.ahrefs.com/analytics.js"
-          data-key="NKxXf5xGU+N48wLRNt5C+g"
-          async
-          strategy="beforeInteractive"
-        />
-      </head>
       <body className={inter.className}>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-4PSKX5EG6M"
