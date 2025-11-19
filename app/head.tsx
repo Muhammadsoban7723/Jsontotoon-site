543578b84ddbb99708fc05e7d903e12a12237ee8
Muhammadsoban7723 <muhammadsoban8090@gmail.com>
2025-11-19 15:33:29 +0500

    remove ahref key

--- app/head.tsx
+++ app/head.tsx
@@ -1,11 +1,4 @@
 export default function Head() {
-  return (
-    <>
-      <meta
-        name="ahrefs-site-verification"
-        content="d602a92b35f7c67aaa9b9a14c00e5ecae5129902b86b0a75f768076fe4772bed"
-      />
-    </>
-  )
+  return null
 }
 
